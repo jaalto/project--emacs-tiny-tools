95f362f10cd02c7e39c55b708da1d4a7f919555f
Jari Aalto <jari.aalto@cante.net>
2008-01-26 18:31:15 +0200

    tinydesk.el: Fox TinyDesk state mode commands: C-c l and C-c n
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- lisp/tiny/tinydesk.el
+++ lisp/tiny/tinydesk.el
@@ -626,9 +626,13 @@ path, garbage at line...Hooks may check the contents of this.")
 (defsubst tinydesk-read-word ()
   "Read filename word."
   ;;   Windows use spaces in file names
-  (ti::remove-properties
-   (ti::string-remove-whitespace
-    (ti::buffer-read-word "- a-zA-Z0-9_/.!@#%&{}[]+:;~`<>"))))
+  (let ((word (ti::remove-properties
+               (ti::string-remove-whitespace
+                (ti::buffer-read-word "- a-zA-Z0-9_/.!@#%&{}[]+:;~`<>")))))
+    ;;  Remove comments from the end
+    (if (string-match "\\(.+[^ \t]\\);;" word)
+        (match-string 1 word)
+      word)))
 
 ;;; ----------------------------------------------------------------------
 ;;;
@@ -650,6 +654,15 @@ path, garbage at line...Hooks may check the contents of this.")
 (defsubst tinydesk-mode-map-activate ()
   "Use local \\{tinydesk-mode-map} on this buffer."
   (use-local-map tinydesk-mode-map))
+
+;;; ----------------------------------------------------------------------
+;;;
+(defun tinydesk-add-space-if-non-space ()
+  "Add extra space if previous character is non-space to make room."
+  (let* ((prev (char-to-string (char-after (1- (point))))))
+    (unless (string-match "[ \t]" prev)
+      ;;  So that comment doe snot get inserted next to the filename
+      (insert " "))))
 
 ;;; ----------------------------------------------------------------------
 ;;;
@@ -867,22 +880,23 @@ TinyDesk: Can't do state autosave: [%s] is not writable." save-to))
   (interactive)
   (let* ((c-chars       tinydesk-:comment-characters)
          (c-lev         tinydesk-:comment-start-level)
-         (c-re          (tinydesk-comment-re)))
+         (c-re          (tinydesk-comment-re))
+         beg)
     (tinydesk-clear-region-properties (point-min) (point-max))
     (save-excursion
       (ti::pmin)
       (while (not (eobp))
-
         ;;  Skip over BOL comments
-        (if (and (not (looking-at (concat "^[ \t]*[" c-chars "]+\\|$")))
-                 (looking-at c-re)
-                 (match-beginning c-lev))
-            (delete-region (match-beginning c-lev) (line-end-position)))
+        (when (and (not (looking-at (concat "^[ \t]*[" c-chars "]+\\|$")))
+                   (looking-at c-re)
+                   (setq beg (match-beginning c-lev)))
+          (goto-char beg)
+          (skip-chars-backward "[ \t]") ; delete leading whitespace too
+          (delete-region (point) (line-end-position)))
         (forward-line 1)))
     (set-buffer-modified-p nil)
-    (message "TinyDesk: *properties cleared from buffer")
-    ;;  Little nasty, but Emacs does not update display always...
-    (redraw-display)))
+    (if (interactive-p)
+        (message "TinyDesk: properties cleared from buffer"))))
 
 ;;; ----------------------------------------------------------------------
 ;;;
@@ -913,24 +927,23 @@ TinyDesk: Can't do state autosave: [%s] is not writable." save-to))
          (loaded     (get-buffer file)) ;in Emacs already ?
          (comment    (tinydesk-comment))
          (err-col    tinydesk-:message-column))
-    ;;  We need the sleep-for, because moving the mouse
-    ;;  clears the message and user may not notice it.
     (cond
      ((eq p (tinydesk-face 'file-pick))
       (cond
        ((null (file-exists-p text))
-        (message (concat "TinyDesk: File not exist, " text)) (sleep-for 0)
+        (message (concat "TinyDesk: File not exist, " text))
         (tinydesk-clear-line-properties))
        (loaded
-        (message "TinyDesk: File already loaded.") (sleep-for 0)
+        (message "TinyDesk: File already loaded.")
         (tinydesk-clear-line-properties))
        (t
         (find-file-noselect text)
-        (message "TinyDesk: Loaded ok.") (sleep-for 0)
+        (message "TinyDesk: Loaded ok.")
         (tinydesk-clear-line-properties)
         (move-to-column err-col t)
-        (if (not (looking-at "$\\|[ \t]*$"))
-            (end-of-line))
+        (unless (looking-at "$\\|[ \t]*$")
+          (end-of-line))
+        (tinydesk-add-space-if-non-space)
         (insert comment " loaded")
         (beginning-of-line)))))))
 
@@ -942,7 +955,8 @@ Marking is only done if word is valid filename."
   (interactive)
   (save-excursion
     (tinydesk-mark-region
-     (point-min) (point-max)
+     (point-min)
+     (point-max)
      (tinydesk-comment-re)
      tinydesk-:comment-start-level
      (or (interactive-p)
@@ -950,19 +964,9 @@ Marking is only done if word is valid filename."
 
 ;;; ----------------------------------------------------------------------
 ;;;
-(defun tinydesk-set-face-non-files-buffer  ()
-  "Change face to 'error in those lines whose first word is not valid file."
-  (interactive)
-  (tinydesk-set-face-non-files-region (point-min) (point-max))
-  (if (interactive-p)
-      (message "TinyDesk: marked non-lodable files")))
-
-;;; ----------------------------------------------------------------------
-;;;
 (defun tinydesk-set-face-non-files-region (beg end)
-  "Change face to 'error in those lines whose first word is not valid file.
-Also add comment marker for user that do not have highlight capability.
-Region is BEG END."
+  "In region BEG END set 'error face to invalid files (first word).
+Also add textual comment to the end of line if needed."
   (interactive "r")
   (let* ((empty-re      "^[ \t]*$")
          (sub-level     tinydesk-:comment-start-level)
@@ -984,20 +988,29 @@ Region is BEG END."
             (setq word (tinydesk-read-word))
             (if (and word (file-exists-p word))
                 nil
-;;;         (ti::d! word)
+ ;;         (ti::d! word)
               (tinydesk-line-property-set-error) ;put color on line
               ;;  Show to user that does not see the color
               (move-to-column err-col t)
-              ;; Is the filename that long, that it goes past err-col ?
+              ;; Is the filename that long, that it goes past err-col?
               (cond
                ((eq (point) (line-end-position))) ;do nothing
                ((looking-at (concat "[ \t" c-chars "]"))
-                (kill-line))            ;delete other marks
+                (kill-line))      ;delete other marks
                (t                 ;no other choices. place is cccupied
                 (end-of-line)
-                (insert " ")))          ;separate word
+                (insert " ")))    ;separate word
               (insert (concat comment " invalid"))))
           (forward-line 1))))))
+
+;;; ----------------------------------------------------------------------
+;;;
+(defun tinydesk-set-face-non-files-buffer  ()
+  "Change face to 'error in those lines whose first word is not valid file."
+  (interactive)
+  (tinydesk-set-face-non-files-region (point-min) (point-max))
+  (if (interactive-p)
+      (message "TinyDesk: marked non-lodable files")))
 
 ;;; ----------------------------------------------------------------------
 ;;; - This function is not general.
@@ -1081,6 +1094,7 @@ Example:
                 (move-to-column err-col t)
                 (if (not (looking-at (concat "$\\|[ \t" c-chars "]")))
                     (end-of-line)) ;no other choices, place is cccupied
+                (tinydesk-add-space-if-non-space)
                 (insert (concat comment " loaded")))
                (t
                 ;; ............................... not loaded in Emacs ...
