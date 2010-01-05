f4e527ca37f990bb1911eff7f99bddba61d7847e
Jari Aalto <jari.aalto@cante.net>
2010-01-05 16:21:21 +0200

    lisp/unmaintained: update copyright
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- lisp/unmaintained/tinypgp.el
+++ lisp/unmaintained/tinypgp.el
@@ -4,7 +4,7 @@
 
 ;;{{{ Id
 
-;; Copyright (C)    1996-2009 Jari Aalto
+;; Copyright (C)    1996-2010 Jari Aalto
 ;; Keywords:        extensions
 ;; Author:          Jari Aalto
 ;; Maintainer:      Jari Aalto
