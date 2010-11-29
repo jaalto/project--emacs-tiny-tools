df2a167f61446a47a517aad0e79e26f58ce009e1
Jari Aalto <jari.aalto@cante.net>
2010-11-29 14:51:53 +0200

    tinyepkg.el: Started implementing the package.

--- lisp/tiny/tinyepkg.el
+++ lisp/tiny/tinyepkg.el
@@ -2,9 +2,7 @@
 
 ;; This file is not part of Emacs
 
-;;{{{ Id
-
-;; Copyright (C)    2009 Jari Aalto
+;; Copyright (C)    2009-2011 Jari Aalto
 ;; Keywords:        tools
 ;; Author:          Jari Aalto
 ;; Maintainer:      Jari Aalto
@@ -24,20 +22,20 @@
 ;;
 ;; Visit <http://www.gnu.org/copyleft/gpl.html> for more information
 
-;; ....................................................... &t-install ...
+;;; Install:
+
 ;;  Put this file on your Emacs-Lisp `load-path', add following into your
 ;;  ~/.emacs startup file.
 ;;
-;;      (require 'tinyepkg)
+;;      (require 'tinyepkg)		; Requires Emacs 22+
 ;;
 ;;  You can also use the preferred way: autoload
 ;;
 ;;       (autoload 'epackage "tinyepkg" "" t)
 ;;       (global-set-key "\C-cP" 'epackage)
 
-;; ..................................................... &t-commentary ...
 ;;; Commentary:
-;;
+
 ;;  Preface 2009
 ;;
 ;;      Emacs has been around for decades now. Many new version have
@@ -102,7 +100,7 @@
 ;;      type of keys can be used to navigate in the buffer. The
 ;;      *Local* is "yes" when package has been downloaded to local
 ;;      disk. The *Status* indicates if the package activation code is
-;;      found from `ROOT/enabled' directory (see below). User's
+;;      found from `ROOT/install' directory (see below). User's
 ;;      standard Emacs startup files, like ~/.emacs are never
 ;;      modified.
 ;;
@@ -135,22 +133,6 @@
 ;;      and this is done via open internet connection. Install command
 ;;      also requires an open internet connection.
 ;;
-;;	Development ideas:
-;:
-;;	- Git tags, where is this information kept?
-;;	- How to update package, or all packages?
-;;	  => Running git preocess? When update is avilable how to flag this?
-;;	  => What about conflits?
-;;	- What about 'local', manual branch and updates?
-;;	- Retrieve new yellow pages (available packages)
-;;	- Rescan current information? (what is installed, what is not)
-;;        => Keep cache? Or regenerate, or scan at startup every time?
-;;	- What if user manually deletes directories? Refresh?
-;;	- Package health check, Lint?
-;;	- Edit yellow pages catalog?
-;;	  => Submit/update yellow pages catalog changes?
-;;	  => version controlled, patches? Interface to automatic email?
-;;
 ;;  Epackage system layout
 ;;
 ;;      The packages are installed under root `epackage--root-directory',
@@ -158,12 +140,11 @@
 ;;      root directory is organized as follows:
 ;;
 ;;          epackage
-;;          | epackage-loader.el  ;; activate + installed as one big file.
+;;	    | epackage.lst		The yellow pages to available packages
+;;          | epackage-loader.el	activated + installed as one big file
 ;;          |
-;;          +--activated/
+;;          +--install/
 ;;          |  <package>-activate.el files
-;;          |
-;;          +--installed/
 ;;          |  <package>-install.el files
 ;;          |
 ;;          +--vc/     Packages. The Version control repositories.
@@ -432,12 +413,29 @@
 ;;
 ;;          X-Comment: <comment here>
 ;;          X-Maintainer-Homepage: <URL>
+;;
+;; TODO
+;:
+;;	- Git tags, where is this information kept?
+;;	- How to update package, or all packages?
+;;	  => Running git preocess? When update is avilable how to flag this?
+;;	  => What about conflits?
+;;	- What about 'local', manual branch and updates?
+;;	- Retrieve new yellow pages (available packages)
+;;	- Rescan current information? (what is installed, what is not)
+;;        => Keep cache? Or regenerate, or scan at startup every time?
+;;	- What if user manually deletes directories? Refresh?
+;;	- Package health check, Lint?
+;;	- Edit yellow pages catalog?
+;;	  => Submit/update yellow pages catalog changes?
+;;	  => version controlled, patches? Interface to automatic email?
+;;	- Yellow pages URL health check? What to do with broken links?
 
 ;;; Change Log:
 
 ;;; Code:
 
-(defconst epackage-version-time "2010.1129.0644"
+(defconst epackage-version-time "2010.1129.1251"
   "*Version of last edit.")
 
 (defcustom epackage--load-hook nil
@@ -446,13 +444,15 @@
   :group 'Epackage)
 
 (defcustom epackage--sources-url
-  "http://localhost/"
+  "http://cante.net/~jaalto/epackage.lst"
   "URL to the location of available package list. The yellow pages.
-This is a text file that contains information about package names and
-their DVCS urls:
+This is a text file that contains information about package names
+and their DVCS URLs. Empty lines and comment on their own lines
+started with character '#' are ignored:
 
-  PACKAGE-NAME REPOSITORY-URL
-  PACKAGE-NAME REPOSITORY-URL
+  # Comment
+  PACKAGE-NAME REPOSITORY-URL DESCRIPTION
+  PACKAGE-NAME REPOSITORY-URL DESCRIPTION
   ...
 
 An example:
@@ -462,7 +462,7 @@ An example:
 (defcustom epackage--root-directory
   (let (ret)
     (dolist (elt (list
-		  (if (feturep 'xemacs)
+		  (if (featurep 'xemacs)
 		      "~/.xemacs.d"
 		    "~/.emacs.d")
 		  "~/elisp"))
@@ -484,7 +484,404 @@ Directory should not contain a trailing slash."
   :group 'Epackage)
 
 (defvar epackage--directory-name "epackage"
-  "Name of package directory under `epackage--root-directory'.")
+  "Name of package directory under `epackage--root-directory'.
+Use function `epackage-directory' for full path name.")
+
+(defvar epackage--directory-name-vcs "vc"
+  "VCS directory under `epackage--root-directory'.
+Use function `epackage-file-name-vcs-compose' for full path name.")
+
+(defvar epackage--directory-name-install "install"
+  "Install directory under `epackage--root-directory'.")
+
+(defconst epackage--layout-mapping
+  '((activate . "xactivate")
+    (autoload . "autoloads")
+    (enable . "install")
+    (info . "info")
+    (loaddefs . "loaddefs")
+    (uninstall . "uninstall"))
+  "File name mappings under epackage/ directory.
+Format is:
+  '((TYPE . FILENAME) ...)
+
+Used in `epackage-file-name-vcs-directory-control-file'.")
+
+(defvar epackage--initialize-flag nil
+  "Set to t, when epackage has been started. do not touch.")
+
+(defvar epackage--program-git nil
+  "Location of program git(1).")
+
+(defvar epackage--sources-file-name "epackage.lst"
+  "Name of yellow pages file that lists available packages.
+See variable `epackage--sources-url'.")
+
+(defvar epackage--package-control-directory "epackage"
+  "Name of directory inside VCS controlled package.")
+
+(defvar epackage--process-output "*Epackage process*"
+  "Output of `epackage--program-git'.")
+
+(defsubst epackage-file-name-compose (name)
+  "Return path to NAME in epackage directory."
+  (format "%s/%s"
+	  (epackage-directory)
+	  name))
+
+(defsubst epackage-file-name-vcs-compose (package)
+  (format "%s/%s%s"
+	  (epackage-directory)
+	  epackage--directory-name-vcs
+	  (if (string= "" package)
+	      ""
+	    (concat "/" package))))
+
+(defsubst epackage-file-name-vcs-directory ()
+  "Return VCS directory"
+  (epackage-file-name-vcs-compose ""))
+
+(defsubst epackage-file-name-install-directory ()
+  "Return VCS directory"
+  (format "%s/%s"
+	  (epackage-directory)
+	  epackage--directory-name-install))
+
+(defsubst epackage-file-name-vcs-package-control-directory (package)
+  "Return control directory of PACKAGE"
+  (let ((root (epackage-file-name-vcs-compose package)))
+    (format "%s/%s" root epackage--package-control-directory)))
+
+(defun epackage-file-name-vcs-directory-control-file (package type)
+  "Return PACKAGE's control file of TYPE.
+
+TYPE can be on of the following:
+
+  'activate
+  'autoload
+  'enable
+  'info
+  'loaddefs
+  'uninstall
+
+Refer top Epackage specification for more information in
+documentation of tinyepkg.el."
+  (let ((dir (epackage-file-name-vcs-package-control-directory package))
+	(file (cdr-safe (assq type epackage--layout-mapping))))
+    (if (not file)
+	(error "Epackage: Unknown TYPE argument '%s'" type)
+      (cond
+       ((eq type 'info)
+	 (format "%s/%s" dir file))
+       (t
+	(format "%s/%s-%s.el" dir package file))))))
+
+(defsubst epackage-file-name-activated-compose (package)
+  "Return path to PACKAGE under activated directory."
+  (format "%s/%s%s"
+	  (epackage-directory)
+	  epackage--directory-name-install
+	  (if (string= "" package)
+	      ""
+	    (format "/%s-xactivate.el" package))))
+
+(defsubst epackage-file-name-enabled-compose (package)
+  "Return path to PACKAGE under install directory."
+  (format "%s/%s%s"
+	  (epackage-directory)
+	  epackage--directory-name-install
+	  (if (string= "" package)
+	      ""
+	    (format "/%s-install.el" package))))
+
+(defun epackage-package-enabled-p (package)
+  "Return file if PACKAGE is enabled."
+  (let ((file (epackage-file-name-enabled-compose package)))
+    (if (file-exists-p file)
+	file)))
+
+(defun epackage-package-activated-p (package)
+  "Return file if PACKAGE is activated."
+  (let ((file (epackage-file-name-activated-compose package)))
+    (if (file-exists-p file)
+	file)))
+
+(defun epackage-package-downloaded-p (package)
+  "Check if package has been downloaded."
+  (unless (stringp package)
+    (error "Epackage: PACKAGE arg is not a string."))
+  (let ((dir (epackage-file-name-vcs-compose package)))
+    (file-directory-p dir)))
+
+(defun epackage-package-activated-p (package)
+  "Check if package has been activated, return activate file."
+  (unless (stringp package)
+    (error "Epackage: PACKAGE arg is not a string."))
+  (let ((file (epackage-file-name-activated-compose package)))
+    (if (file-exists-p file)
+	file)))
+
+(defun epackage-package-enabled-p (package)
+  "Check if package has been enabled, return enabled file."
+  (unless (stringp package)
+    (error "Epackage: PACKAGE arg is not a string."))
+  (let ((file (epackage-file-name-install-compose package)))
+    (if (file-exists-p file)
+	file)))
+
+(defsubst epackage-file-name-sources-list ()
+  "Return path to `epackage--sources-file-name'."
+  (epackage-file-name-compose epackage--sources-file-name))
+
+(defsubst epackage-sources-list-p ()
+  "Check existence of `epackage--sources-file-name'."
+  (file-exists-p (epackage-file-name-sources-list)))
+
+(defsubst epackage-sources-list-verify ()
+  "Signal error if `epackage--sources-file-name' does not exist."
+  (unless (epackage-sources-list-p)
+    (error "Epackage: Missing file %s. Run epackage-initialize"
+	   (epackage-file-name-sources-list))))
+
+(defun epackage-program-git-verify ()
+  "Verify variable `epackage--program-git'."
+  (when (or (not (stringp epackage--program-git))
+	    (not (file-exists-p epackage--program-git)))
+    (error
+     (substitute-command-keys
+      (format "Epackage: Invalied variable `epackage--program-git' (%s) "
+	      epackage--program-git
+	      "Run \\[epackage-initialize]")))))
+
+(defun epackage-directory ()
+  "Return root directory."
+  (format "%s%s"
+	  (expand-file-name
+	   (file-name-as-directory epackage--root-directory))
+	  (if (stringp epackage--directory-name)
+	      epackage--directory-name
+	    (error "Epackage: epackage--directory-name is not a string"))))
+
+(put  'epackage-with-binary 'lisp-indent-function 0)
+(put  'epackage-with-binary 'edebug-form-spec '(body))
+(defmacro epackage-with-binary (&rest body)
+  "Disable all interfering `write-file' effects and run BODY."
+  `(let ((version-control 'never)
+	 (backup-inhibited t)
+	 (buffer-file-coding-system 'no-conversion)
+	 write-file-functions
+	 after-save-hook)
+     ,@body))
+
+(put  'epackage-with-sources-list 'lisp-indent-function 0)
+(put  'epackage-with-sources-list 'edebug-form-spec '(body))
+(defmacro epackage-with-sources-list (&rest body)
+  "Run BODY in package list buffer."
+  `(progn
+     (epackage-sources-list-verify)
+     (with-current-buffer
+	 (find-file-noselect (epackage-file-name-sources-list))
+       ,@body)))
+
+(defun epackage-git-command-process (&rest args)
+  "Run git COMMAND with output to `epackage--process-output'."
+  (epackage-program-git-verify)
+  (with-current-buffer (get-buffer-create epackage--process-output)
+    (goto-char (point-max))
+    (apply 'call-process
+	   epackage--program-git
+	   (not 'infile)
+	   (current-buffer)
+	   (not 'display)
+	   args)))
+
+(defsubst epackage-git-command-ok-p (status)
+  "Return non-nil if command STATUS was ok."
+  (zerop status))
+
+(defun epackage-git-command-clone (url dir &optional verbose)
+  "Clone git PACKAGE under `epackage--directory-name'.
+If VERBOSE is non-nil, display progress message."
+  (let ((default-directory (epackage-file-name-vcs-directory)))
+    (if verbose
+	(message "Epackage: Running git clone %s %s ..." url git))
+    (prog1
+	(epackage-git-command-ok-p
+	 (epackage-git-command-process
+	  "clone"
+	  url
+	  dir))
+    (if verbose
+	(message "Epackage: Running git clone %s %s ...done" url git)))))
+
+(defun epackage-download-package (package &optional verbose)
+  "Download PACKAGE to VCS directory."
+  (let ((url (epackage-sources-list-info-url package)))
+    (unless url
+      (error "Epackage: No Git URL for package '%s'" package))
+    (let ((dir (epackage-file-name-vcs-compose package)))
+      (epackage-git-command-clone url dir))))
+
+(defun epackage-enable-package (package)
+  "Enable PACKAGE."
+  (let ((from (epackage-file-name-vcs-directory-control-file
+	       package 'enable))
+	(to (epackage-file-name-install-compose package)))
+    (unless (file-exists-p from)
+      (error "Epackage: file does not exists: %s" from))
+    (copy-file from to 'overwrite 'keep-time)))
+
+(defun epackage-activate-package (package)
+  "Activate PACKAGE."
+  (let ((from (epackage-file-name-vcs-directory-control-file
+	       package 'activate))
+	(to (epackage-file-name-activated-compose package)))
+    (unless (file-exists-p from)
+      (error "Epackage: file does not exists: %s" from))
+    (copy-file from to 'overwrite 'keep-time)))
+
+(defun epackage-enable-package (package)
+  "Activate PACKAGE."
+  (let ((from (epackage-file-name-vcs-directory-control-file
+	       package 'enable))
+	(to (epackage-file-name-enabled-compose package)))
+    (unless (file-exists-p from)
+      (error "Epackage: file does not exists: %s" from))
+    (copy-file from to 'overwrite 'keep-time)))
+
+(defun epackage-disable-package (package)
+  "Disable PACKAGE."
+  (dolist (file (directory-files
+		 dir
+		 'full-path
+		 (format "^%s-.*\\.el" package)
+		 t))
+    (if (file-exists-p file)
+	(delete-file file))))
+
+(defun epackage-action-package (package action)
+  "Perform ACTION on PACKAGE.
+ACTION can be:
+
+  'enable
+  'disable
+  'activate
+  'uninstall"
+  ;; FIXME: Not implemented
+  )
+
+(defun epackage-generate-loader-file ()
+  "Generate main loader for all installed or activated packages."
+  (let ((root (epackage-file-name-vcs-directory)))))
+
+(defun epackage-sources-list-info-main (package)
+  "Return '(pkg url description) for PACKAGE.
+Format is described in variable `epackage--sources-url'."
+  (epackage-with-sources-list
+   (goto-char (point-min))
+   (let ((re
+	  (format
+	   `,(concat "^\\(%s\\)\\>"
+		     "[ \t]+\\([^ \t\r\n]+\\)"
+		     "[ \t]*\\([^ \t\r\n]*\\)")
+	   (regexp-quote package))))
+     (when (re-search-forward re nil t)
+       (list
+	(match-string-no-properties 1)
+	(match-string-no-properties 2)
+	(match-string-no-properties 3))))))
+
+(defun epackage-sources-list-info-url (package)
+  "Return URL for PACKAGE."
+  (let ((info (epackage-sources-list-info-main package)))
+    (when info
+      (nth 1 info))))
+
+(defun epackage-require-emacs ()
+  "Require Emacs features."
+  (unless (fboundp 'url-retrieve-synchronously)
+    (error (concat
+	    "Epackage: this Emacs does not define "
+	    "`url-retrieve-synchronously' which is included in url.el"))))
+
+(defun epackage-require-git ()
+  "Require Git program."
+  (cond
+   ((null epackage--program-git)
+    (let ((bin (executable-find "git")))
+      (unless bin
+	(error "Epackage: program 'git' not found in PATH"))
+      (setq epackage--program-git bin)))
+   ((and (stringp epackage--program-git)
+	 (not (file-exists-p epackage--program-git)))
+    (error "Epackage: [ERROR] Invalid `epackage--program-git' (%s)"
+	   epackage--program-git))
+   ((file-executable-p epackage--program-git)) ;All ok
+   (t
+    (error "Epackage: [ERROR] Unknown value in `epackage--program-git' (%s)"
+	   epackage--program-git))))
+
+(defun epackage-require-directories ()
+  "Buid directory structure."
+  (dolist (dir (list
+		(epackage-directory)
+		(epackage-file-name-vcs-directory)
+		(epackage-file-name-install-directory)))
+    (unless (file-directory-p dir)
+      (message "Epackage: Making directory %s ..." dir)
+      (make-directory dir))))
+
+(defun epackage-require-main ()
+  "Check requirements to run Epackage."
+  (epackage-require-emacs)
+  (epackage-require-git)
+  (epackage-require-directories))
+
+(defun epackage-url-http-parse-respons-error (&optional url)
+  "On HTTP GET error, show reponse and signal error for optional URL."
+  (let ((status (url-http-parse-response)))
+    (when (or (< status 200)
+	      (>= status 300))
+      (display-buffer (current-buffer))
+      (error "HTTP access error %d%s"
+	     status
+	     (if url
+		 (concat " " url)
+	       "")))))
+
+(defun epackage-url-retrieve-main (url file)
+  "Download URL and save to a FILE."
+  (let ((buffer (url-retrieve-synchronously url)))
+    (unless buffer
+      (error "Epackage: can't access %s" url))
+    (with-current-buffer buffer
+      (epackage-url-http-parse-respons-error url)
+      (re-search-forward "^$" nil 'move)
+      (forward-char)
+      (epackage-with-binary
+	(write-region (point) (point-max) file)
+	(kill-buffer (current-buffer))))))
+
+(defun epackage-url-retrieve-sources-list (&optional message)
+  "Download package list file, the yellow pages."
+  (if message
+      (message message))
+  (epackage-url-retrieve-main
+   epackage--sources-url
+   (epackage-file-name-sources-list)))
+
+(defun epackage-cmd-download-sources-list ()
+  "Download package list; the yellow pages of packages."
+  (interactive)
+  (epackage-url-retrieve-sources-list "Downloading package sources list"))
+
+(defun epackage-initialize ()
+  "Inialize package."
+  (unless epackage--initialize-flag
+    (epackage-require-main))
+  (unless (epackage-sources-list-p)
+    (epackage-cmd-download-sources-list))
+  (setq epackage--initialize-flag t))
 
 ;;;###autoload (autoload 'epackage-mode          "epackage" "" t)
 ;;;###autoload (autoload 'turn-on-epackage-mode  "epackage" "" t)
