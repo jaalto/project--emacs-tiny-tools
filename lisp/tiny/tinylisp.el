cd108a325748a1b10f0ad4a31b62dd7e4825008c
Jari Aalto <jari.aalto@cante.net>
2019-05-25 18:57:21 +0300

    lisp/tiny/tinylisp.el: Correct byte compilation

--- lisp/tiny/tinylisp.el
+++ lisp/tiny/tinylisp.el
@@ -1896,7 +1896,8 @@ Following variables are set during BODY:
 
 `dir'      Directrory name
 `dir-list' All directories under `dir'."
-  `(cl-flet ((recurse
+  ;; Cannot use `cl-flet' which is lexical
+  `(flet ((recurse
 	      (dir)
 	      (let ((dir-list (tinylisp-directory-list dir)))
 		,@body
@@ -2833,7 +2834,7 @@ Input:
             (setq func (cdr func))      ;("defun" . "t1")
             ;;  elp can only insrument functions
             (when (string-match "defun\\|defsubst" type)
-              (incf count)
+              (cl-incf count)
               (tinylisp-symbol-do-macro func nil
 		(elp-restore-function func) ;do this first
 		(if (null remove)
@@ -3179,7 +3180,7 @@ DebugTag: 22-56 file.el
       (if remove
           (while (re-search-forward tag nil t)
             (if verb (message "TinyLisp:  uninstrumenting tag %d" i))
-            (incf  i)
+            (cl-incf i)
             (beginning-of-line)
             (kill-line 1))
         (when (or (null (re-search-forward tag nil t))
@@ -3196,7 +3197,7 @@ DebugTag: 22-56 file.el
             (forward-line 1)
             (if verb
                 (message "TinyLisp:  instrumenting tag %d" i))
-            (incf   i)))))
+            (cl-incf  i)))))
     (when (and verb (not (zerop i)))
       (if remove
           (message "TinyLisp: Debug tags removed.")
@@ -4277,7 +4278,7 @@ Return:
 	    (format "%s ..." (ti::string-left (prin1-to-string pkg) 80)))))
 	(if verb
 	    (message "TinyLisp: lib info %d/%d %s" i max name))
-	(incf  i)
+	(cl-incf i)
 	(setq dep-list  nil
 	      pkg       nil)))
     (tinylisp-with-current-buffer buffer
@@ -4557,7 +4558,7 @@ NOTE
               (progn
                 (setq elt (file-name-nondirectory elt))
                 (load elt 'noerr)))
-          (incf count))
+          (cl-incf count))
          (t
           (message "TinyLisp: Reload failed %s" elt)))))
     (when verb
@@ -5486,7 +5487,7 @@ User can't see string echoed otherwise. Optionally RESTORE."
       (if (> (length ovl) 1)
           (setq prefix-ok t))
       (dolist (elt ovl)
-        (incf  count)
+        (cl-incf count)
         (setq ov-str
               (format
                "%sov%s%s "
