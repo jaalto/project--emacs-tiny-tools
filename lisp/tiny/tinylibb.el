1f8ebc50ca612b903f9dd8c5e4aeb9b16c0af404
Jari Aalto <jari.aalto@cante.net>
2024-11-08 12:43:02 +0200

    tinylibb.el: (define-advice): New 25.x compatibility

--- lisp/tiny/tinylibb.el
+++ lisp/tiny/tinylibb.el
@@ -81,7 +81,7 @@
 (eval-and-compile
   (autoload 'ti::replace-match "tinylibm"))
 
-(defconst tinylibb-version-time "2023.0918.1858"
+(defconst tinylibb-version-time "2024.1108.1035"
   "Latest version number as last modified time.")
 
 ;;; ....................................................... &emulation ...
@@ -204,6 +204,9 @@ PAD says to padd hex string with leading zeroes."
 (defun-maybe byte-compiling-files-p ()
   "Return t if currently byte-compiling files."
   (string= (buffer-name) " *Compiler Input*"))
+
+(defmacro-maybe define-advice (&rest args) ;; Emacs 25.x
+  `(defadvice ,@args))
 
 (defmacro-maybe cl-flet (&rest args)
   `(cl-flet ,@args))
